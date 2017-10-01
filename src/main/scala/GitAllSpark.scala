f5023d05b626f179091d00b146f13d5f0404f555
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-10-01 18:36:45 -0500

    first working version of distributed checkout of all hashes on Spark

--- src/main/scala/GitAllSpark.scala
+++ src/main/scala/GitAllSpark.scala
@@ -4,14 +4,10 @@
 
 package se_hpc
 
-//import blockperf._
 import org.apache.spark.SparkContext
-import org.apache.spark.input.PortableDataStream
-import org.apache.spark.rdd.RDD
-import scala.util.{ Try, Success, Failure }
-import java.io._
-//import breeze.linalg._
-import org.json4s.jackson.JsonMethods._
+
+import ammonite.ops._
+import ammonite.ops.ImplicitWd._
 import org.json4s.JsonDSL._
 import java.net.InetAddress
 
@@ -19,40 +15,68 @@ object GitAllSparkScala {
 
   def main(args: Array[String]) {
     val config = parseCommandLine(args).getOrElse(Config())
-    val experiment = Experiment("simplemap-spark-scala")
+    val experiment = Experiment("git-all-spark-scala")
     val sc = new SparkContext()
+    val repoURL = config.url.getOrElse("")
+    val sourceFolder = config.src.getOrElse("")
+    val destinationFolder = config.dst.getOrElse("")
 
-    val rdd = sc.parallelize(1 to config.files, config.nodes * config.cores)
+    // These defaults are for use on the Cooley cluster...
+    val srcRoot = Path(new java.io.File(config.srcRoot.getOrElse("/projects/SE_HPC")))
+    //val dstRoot = Path(new java.io.File(config.dstRoot.getOrElse("/scratch/SE_HPC")))
 
-    val rdd2 = rdd.map { i => makeScratchDir(i).loc }
+    //Establishes path for source folder where clone occurs and destination folder which will recieve every commit
+    val sourcePath = srcRoot / sourceFolder
 
-    val sumLOC = rdd2.reduce(_ + _)
-    println(s"cumulative LOC = $sumLOC")
-  }
+    // Initial clone takes place on the driver.
+    // The hash fetches take place on the nodes.
+    if (exists ! srcRoot) {
+      println("Source folder already exists - might be ok")
+    } else {
+      println("Creating non-existent root " + srcRoot.toString)
+      mkdir ! srcRoot
+    }
 
-  case class Info(hostname: String, path: String, loc: Int)
+    //Clones repo into source folder
+    if (config.gitClone) {
+      System.out.println("Cloning to " + srcRoot.toString)
+      %.git("clone", repoURL)(srcRoot)
+    }
+    val rdd = sc.parallelize(hashCodes(srcRoot, sourceFolder), config.nodes * config.cores)
 
-  def makeScratchDir(id: Int): Info = {
-    import ammonite.ops._
-    import ammonite.ops.ImplicitWd._
+    val rdd2 = rdd.map { hash => doGitClone(config, hash).toString }
 
-    val path = root / "scratch" / "SE_HPC"
-    mkdir ! path
-    val path2 = root / "scratch" / "SE_HPC" / id.toString
-    mkdir ! path2
-    val filePath = path2 / "AmmoniteExample.scala"
-    println(s"Writing to $filePath")
-    write(filePath, """|object amm extends App {
-                       |   ammonite.Main().run()
-                       |}
-                       |""".stripMargin('|'))
-
-    println(s"wc -l $filePath")
-    val result = %%('wc, "-l", filePath)
+    val result = rdd2.reduce(_ + "\n" + _)
 
     println(result)
-    val crudeLOC = result.out.string.split(" ")(0).toInt
-    Info(InetAddress.getLocalHost().getHostName(), path2.toString, crudeLOC)
+
+  }
+
+  case class Info(hostname: String, path: String)
+
+  def doGitClone(config: Config, hash: String): Info = {
+    val srcRoot = Path(new java.io.File(config.srcRoot.getOrElse("/projects/SE_HPC")))
+    val sourceFolder = config.src.getOrElse("")
+    val sourcePath = srcRoot / sourceFolder
+    val destRoot = Path(new java.io.File(config.dstRoot.getOrElse("/scratch/SE_HPC")))
+    val destinationFolder = config.dst.getOrElse(InetAddress.getLocalHost().getHostName())
+    val destinationPath = destRoot / destinationFolder
+
+    if (!(exists ! destinationPath)) {
+      mkdir ! destinationPath
+    }
+    mkdir ! destinationPath / hash
+    var currentPath = destinationPath / hash
+    %.git('init)(currentPath)
+    %%("git", "remote", "add", "upstream", sourcePath)(currentPath)
+    %%("git", "fetch", "upstream")(currentPath)
+    %%("git", "checkout", hash)(currentPath)
+    if (config.cloc) {
+      val lines = %%("cloc", currentPath)(destinationPath)
+      println(lines)
+    }
+    val commitHashPath = destinationPath / hash
+    Info(InetAddress.getLocalHost.getHostName, commitHashPath.toString)
   }
 
   def parseCommandLine(args: Array[String]): Option[Config] = {
@@ -61,15 +85,9 @@ object GitAllSparkScala {
       opt[String]('s', "src") action { (x, c) =>
         c.copy(src = Some(x))
       } text ("s/src is a String property")
-      opt[Unit]('g', "generate") action { (_, c) =>
-        c.copy(generate = true)
-      } text ("g/generate is a Boolean property")
       opt[String]('d', "dst") action { (x, c) =>
         c.copy(dst = Some(x))
       } text ("d/dst is a String property")
-      opt[Int]('f', "files") action { (x, c) =>
-        c.copy(files = x)
-      } text ("f/files is an int property")
       opt[Int]('n', "nodes") action { (x, c) =>
         c.copy(nodes = x)
       } text ("n/nodes is an int property")
@@ -82,9 +100,34 @@ object GitAllSparkScala {
       opt[String]('x', "xml") action { (x, c) =>
         c.copy(xmlFilename = Some(x))
       } text (s"xml <filename> is where to write XML reports")
+      opt[String]("src-root") action { (x, c) =>
+        c.copy(srcRoot = Some(x))
+      } text ("srcRoot is a base directory for cloning stuff")
+      opt[String]("dst-root") action { (x, c) =>
+        c.copy(dstRoot = Some(x))
+      } text ("dstRoot is a base directory for fetching hashes")
+      opt[String]('u', "url") action { (x, c) =>
+        c.copy(url = Some(x))
+      } text ("u/url is a String property")
+      opt[Unit]("cloc") action { (x, c) =>
+        c.copy(cloc = true)
+      }
+      opt[Unit]("git-clone") action { (x, c) =>
+        c.copy(gitClone = true)
+      }
       help("help") text ("prints this usage text")
     }
     parser.parse(args, Config())
+  }
+
+  def hashCodes(rootPath: Path, args: String): Array[String] = {
+    val source = rootPath / args
+    val log = %%("git", "log")(source)
+    val logString = log.toString
+    val logArray = logString.split("\n")
+    val justHashCodes = logArray filter { line => line.startsWith("commit") } map { line => line.split(" ")(1) }
+
+    return justHashCodes
   }
 
   case class Experiment(name: String) {
@@ -112,14 +155,22 @@ object GitAllSparkScala {
   case class Config(
       src: Option[String] = None,
       dst: Option[String] = None,
+      cores: Int = 12,
       generate: Boolean = false,
       blocks: Int = 1,
       blockSize: Int = 1, // 1 MB
+      nparts: Int = 1,
+      size: Int = 1,
       nodes: Int = 1,
-      cores: Int = 12,
-      files: Int = 1,
       jsonFilename: Option[String] = None,
-      xmlFilename: Option[String] = None
+      xmlFilename: Option[String] = None,
+      srcRoot: Option[String] = None,
+      dstRoot: Option[String] = None,
+      url: Option[String] = None,
+      start: Option[Int] = None,
+      stride: Option[Int] = None,
+      cloc: Boolean = false,
+      gitClone: Boolean = false
   ) {
 
     def toXML(): xml.Elem = {
@@ -130,7 +181,8 @@ object GitAllSparkScala {
         <property key="generate" value={ generate.toString }/>
         <property key="blocks" value={ blocks.toString }/>
         <property key="blockSize" value={ blockSize.toString } unit="MB"/>
-        <property key="files" value={ files.toString }/>
+        <property key="nparts" value={ nparts.toString }/>
+        <property key="size" value={ size.toString }/>
         <property key="nodes" value={ nodes.toString }/>
         <property key="json" value={ jsonFilename.getOrElse("") }/>
         <property key="xml" value={ xmlFilename.getOrElse("") }/>
@@ -141,7 +193,7 @@ object GitAllSparkScala {
       val properties = ("src" -> src.getOrElse("")) ~ ("dst" -> dst.getOrElse("")) ~ ("cores" -> cores.toString) ~
         ("generate" -> generate.toString) ~ ("blocks" -> blocks.toString) ~ ("blockSize" -> blockSize.toString) ~
         ("blockSizeUnit" -> "MB") ~
-        ("files" -> files.toString) ~ ("nodes" -> nodes.toString) ~
+        ("nparts" -> nparts.toString) ~ ("size" -> size.toString) ~ ("nodes" -> nodes.toString) ~
         ("jsonFilename" -> jsonFilename.getOrElse("")) ~ ("xmlFilename" -> xmlFilename.getOrElse(""))
       ("config" -> properties)
     }
