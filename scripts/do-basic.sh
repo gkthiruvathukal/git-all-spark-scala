f3e930da41acd412766b0ef31221349eaf0ea5e8
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-12-08 03:36:13 +0000

    various fixes to workflow script

--- scripts/do-basic.sh
+++ scripts/do-basic.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 #
-# do-basic.sh: Not a great name but this shows how to start Spark within a Cobalt job and
-#   run the actual application (.jar file) on the nodes.
-#
-#   You can pass any arguments to the Spark job. These are passed in their entirety.
-
-# Start (Cobalt Job-Specific) Apache Spark Cluster
+# Example of a workflow script to schedule and run Apache Spark + the user-specific Spark application
+# Only works on Cooley, most likely.
 #
 
-JOB_LOG=$HOME/logs/$COBALT_JOBID.txt
-JOB_JSON=$HOME/logs/$COBALT_JOBID.json
-JOB_XML=$HOME/logs/$COBALT_JOBID.xml
+# Point to the shared build of Spark. In this case, I have my own build, but it's read-only.
 
 if [ -z "${SPARK_HOME}" ]; then
    export SPARK_HOME=$HOME/code/spark
 fi
 
-if [ -z "${SPARK_CONF_DIR}" ]; then
-   export SPARK_CONF_DIR=${SPARK_HOME}/conf
-fi
+# The slaves file must be specific to this job ${COBALT_JOBID}
 
-export SPARK_SLAVES=${SPARK_CONF_DIR}/slaves.${COBALT_JOBID}
-
+export SPARK_SLAVES=${HOME}/spark-state/slaves.${COBALT_JOBID}
 cp $COBALT_NODEFILE $SPARK_SLAVES
 
+# These are log files specific to my application
+
+JOB_LOG=$HOME/logs/$COBALT_JOBID.txt
+JOB_JSON=$HOME/logs/$COBALT_JOBID.json
+JOB_XML=$HOME/logs/$COBALT_JOBID.xml
+
+# Check /scratch on each node
+
+echo "Report of /scratch usage on nodes" >> $JOB_LOG
+for host in $(cat $COBALT_NODEFILE)
+do
+   ssh $host "hostname; df -h /scratch" >> $JOB_LOG
+   ssh $host "rm -rf /scratch/SE_HPC/*" >> $JOB_LOG
+done
+
+
+# Start Spark on the allocated nodes
+# Note: We probably can do $SPARK_HOME/sbin/start-all.sh here
+
 pushd $SPARK_HOME
-echo "Spark Slaves File ${SPARK_SLAVES}" >> $JOB_LOG
-cat $SPARK_SLAVES >> $JOB_LOG
+#echo "Spark Slaves File ${SPARK_SLAVES}" >> $JOB_LOG
+#cat $SPARK_SLAVES >> $JOB_LOG
 ./sbin/start-all.sh
-NODES=`wc -l ${SPARK_SLAVES} | cut -d" " -f1`
 popd
 
+NODES=`wc -l ${SPARK_SLAVES} | cut -d" " -f1`
 MASTER=`hostname`
 
 echo "# Spark is now running with $NODES workers:" >> $JOB_LOG
@@ -40,21 +50,16 @@ echo "export SPARK_MASTER_URI=spark://$MASTER:7077" >> $JOB_LOG
 
 SPARK_MASTER_URI=spark://$MASTER:7077
 
-#
-# Done Initializing Apache Spark
-#
-#
-# Submit Application on Spark
-#
+if [ -f spark-defaults.conf ]; then
+   ADD_SPARK_DEFAULTS_CONF="--properties-file $(pwd)/spark-defaults.conf"
+fi
 
-for ASSEMBLY in $(find . -type f -name git-all-spark-scala-assembly*.jar)
+# Run my Apache Spark application code
+
+for ASSEMBLY in $(find . -type f -name *.jar)
 do
-   echo "Running: "$SPARK_HOME/bin/spark-submit --master $SPARK_MASTER_URI $ASSEMBLY "$@" >> $JOB_LOG
-
-   $SPARK_HOME/bin/spark-submit --master $SPARK_MASTER_URI $ASSEMBLY "$@" >> $JOB_LOG
-
+   echo "Running: "$SPARK_HOME/bin/spark-submit $ADD_SPARK_DEFAULTS_CONF --master $SPARK_MASTER_URI $ASSEMBLY "$@" >> $JOB_LOG
+   $SPARK_HOME/bin/spark-submit $ADD_SPARK_DEFAULTS_CONF --master $SPARK_MASTER_URI $ASSEMBLY "$@" >> $JOB_LOG
 done
 
-#
-# Done Submitting Application on Spark
-#
+
