1602cc2e61a84ce7f2aaad09e61d8ded264f112b
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-12-09 05:18:50 +0000

    updates to generate each job as a separate script

--- scripts/generate.py
+++ scripts/generate.py
@@ -2,21 +2,13 @@ import time
 import math
 import sys
 import argparse
+import os
 
-HEADER = """#/bin/bash
-# This script is generated.
-#
-"""
+HEADER = "#/bin/bash\n"
 
 QSUB = """
 
-# repo: %(github)s
-# nodes: %(nodes)s
-# cores: %(cores)s
-# start: %(start)s
-# stride: %(stride)s
-
-qsub -n %(nodes)s -t %(qsub_time)s -A SE_HPC -q pubnet \\
+qsub -n %(nodes)s -t %(qsub_time)s -A %(allocation)s -q %(queue)s --notify %(email)s \\
 	./scripts/do-basic.sh \\
 	--github "%(github)s" --git-clone --checkout \\
 	--src-root /projects/SE_HPC --dst-root "/scratch/SE_HPC" --src "%(src)s" --dst "%(dst)s" --start %(start)s --stride %(stride)s \\
@@ -44,19 +36,29 @@ def get_argparse():
     parser.add_argument('--start_range', type=int, nargs='+', help="start, end", default=[0,1])
     parser.add_argument('--stride', type=int,
                         help="stride (skip) commits", default=1)
+    parser.add_argument('--name', required=True, help="experiment name")
+    parser.add_argument('--allocation', default="SE_HPC", help="allocation")
+    parser.add_argument('--email', default="gkt@cs.luc.edu", help="e-mail to notify")
+    parser.add_argument('--queue', default="pubnet", help="queue (use pubnet or debug)")
+   
 
     return parser
 
+FILENAME="%(name)s-n%(nodes)s-c%(cores)s-%(start)s-%(stride)s.sh"
 
 def generate():
     parser = get_argparse()
     args = parser.parse_args()
+    name = args.name
     min_nodes = args.min_nodes
     max_nodes = args.max_nodes
     cores = args.cores
     fudge = args.fudge
     org = args.org
     repo = args.repo
+    email = args.email
+    queue = args.queue
+    allocation = args.allocation
     src = repo
     dst = repo + "-commits"
     github = "/".join([org, repo])
@@ -64,7 +66,6 @@ def generate():
     stride = args.stride
     seconds = args.max_hours * 60 * 60
 
-    print(HEADER)
     min_log = int(math.log(min_nodes, 2))
     max_log = int(math.log(max_nodes, 2))
     min_log = max(1, min_log)
@@ -80,8 +81,10 @@ def generate():
             start_range = [start_range[0], start_range[0]+1]
 
         for start in range(start_range[0], start_range[1]):
-            print(QSUB % vars())
-
+            with open(FILENAME % vars(), "w") as outfile:
+               outfile.write(HEADER)
+               outfile.write(QSUB % vars())
+            os.chmod(FILENAME % vars(), 0o755)
 
 if __name__ == '__main__':
     generate()
